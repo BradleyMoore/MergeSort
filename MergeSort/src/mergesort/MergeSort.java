4cee926ef40c74f84581c8db69737e0dc1790761
Bradley Moore <BradleyMooreTX@gmail.com>
2013-10-08 12:06:38 -0600

    added comments and removed loopLimit reduction

--- MergeSort/src/mergesort/MergeSort.java
+++ MergeSort/src/mergesort/MergeSort.java
@@ -19,16 +19,16 @@ public class MergeSort {
         int inputLength = unsortedArray.length;
         int loopLimit = inputLength/2;
 
+        // split array into 2 arrays that diff by <= 1
         int[] arrayA = Arrays.copyOfRange(unsortedArray, 0, inputLength/2);
         int[] arrayB = Arrays.copyOfRange(unsortedArray, inputLength/2, inputLength);
         int[] sortedArray = new int[inputLength];
         
-        if (arrayA.length > 1 && arrayB.length > 2) {
+        // continue to split arrays until they are both diff by <= 1
+        if (arrayA.length > 1 && arrayB.length > 1) {
             mergeSort(arrayA);
             mergeSort(arrayB);
         }
-
-        if (inputLength % 2 != 0) loopLimit--;
 
         int i = 0;
         int j = 0;
@@ -40,20 +40,3 @@ public class MergeSort {
             } else {
                 sortedArray[k] = arrayB[j];
                 j++;
-            }
-            k++;
-        }
-
-
-        for (; k < sortedArray.length; k++) {
-            if (i>j) {
-                sortedArray[k] = arrayB[j];
-                j++;
-            } else {
-                sortedArray[k] = arrayA[i];
-                j++;
-            }
-        }
-        return sortedArray;
-    }
-}
